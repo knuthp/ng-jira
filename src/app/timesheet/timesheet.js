d83f67046b71c46de5dcea05edd12f4fef280738
Knut H Pedersen <p72@storebrand.no>
2014-10-29 22:41:02 +0100

    Added hardcoded search and retrieve

--- src/app/timesheet/timesheet.js
+++ src/app/timesheet/timesheet.js
@@ -13,12 +13,34 @@
         });
     }]);
 
-    app.controller('TimesheetController', ['$scope', function ($scope) {
+    app.controller('TimesheetController', ['$scope', '$http', function ($scope, $http) {
+        $scope.issuesInPeriod = [];
 
         var init = function() {
+            console.log("TimesheetController");
         };
 
         init();
+
+
+        $http.get("/rest/api/latest/search?jql=updated>=2014-10-29+and+project=JRA")
+            .success(function(data) {
+                console.log("Total: " + data.total + "l " + data.issues.length);
+                for (var i = 0; i < data.issues.length; i++) {
+                    var issue = data.issues[i];
+                    var x = readIssue(issue);
+                }
+            });
+
+        function readIssue(issue) {
+            $http.get("rest/api/latest/issue/" + issue.key)
+                .success(function(data) {
+                console.log("Data.key:" + data.key);
+                console.log("Project" + data.fields.project.key);
+                $scope.issuesInPeriod.push(data);
+            });
+            return 0;
+        }
     }]);
 
 }(angular.module("ngJira.timesheet", [
